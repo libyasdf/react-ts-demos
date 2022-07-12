d81b9c5e66f2c47b7e24d3ac5ea9ab276097b118
libyasdf <liby1994@126.com>
2022-07-12 21:08:44 +0800

    instance

--- src/axios/Axios.tsx
+++ src/axios/Axios.tsx
@@ -2,4 +2,7 @@ export default class Axios {
     // constructor(parameters) {
         
     // }
+    request(){
+        
+    }
 }
--- src/axios/index.tsx
+++ src/axios/index.tsx
@@ -1,5 +1,13 @@
 import Axios from './Axios';
 
 function createInstance() {
-    let context = new Axios();
+    let context = new Axios(); // this 指针上下文
+    // 让request方法里的this永远指向new Axios()
+    let instance = Axios.prototype.request.bind(context);
+    instance = Object.assign(instance, Axios.prototype, context);
+    return instance;
 }
+
+let axios = createInstance();
+
+export default axios;
